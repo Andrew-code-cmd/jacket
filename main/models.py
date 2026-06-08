05f4bc907e8ace0def15d72cd9a2eb6fd3f9cb25
Andrey <andrey.khlebov@bk.ru>
2026-06-08 21:57:01 +0700

    made a catalog, filters, product_detail, search

--- main/models.py
+++ main/models.py
@@ -28,7 +28,7 @@ class Size(models.Model):
         return self.name
     
 class ProductSize(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_sizes')
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     #доступное количество для покупки 
     stock = models.PositiveIntegerField(default=0)
